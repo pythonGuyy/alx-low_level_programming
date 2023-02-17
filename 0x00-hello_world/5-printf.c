c8f49753ee4499c0111f26ec1efa555d2737c5b7
pythonGuyy <olumordaniel1@gmail.com>
2023-02-16 17:19:50 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that printsa line using the prinf function
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
