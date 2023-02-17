cdcb92575066273aefe4de2842d525c4a9009102
pythonGuyy <olumordaniel1@gmail.com>
2023-02-16 18:26:48 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - a program that prints a line to the standard error
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
