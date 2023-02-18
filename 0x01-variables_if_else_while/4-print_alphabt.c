f1bcba37fd0e9fe9cad7ef2922db476c3180cf2a
pythonGuyy <olumordaniel1@gmail.com>
2023-02-17 18:49:47 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints the alphabet in lowercase
- * followed by a new line, except q and e
+ * followed  by a new line, except q and e
  * Return: Always 0
  */
 int main(void)
@@ -14,5 +14,4 @@ int main(void)
 			putchar(la);
 	}
 	putchar('\n');
-a	return (0);
-}
+	return (0);
