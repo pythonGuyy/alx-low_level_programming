759e7bd179a566f95c01791cbd2433bbed360678
pythonGuyy <olumordaniel1@gmail.com>
2023-02-17 18:09:37 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main- Entry
+ * main - Entry
  * Return: Always 0
  */
 int main(void)
@@ -27,3 +27,4 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
+}
